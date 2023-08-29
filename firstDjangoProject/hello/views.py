d22ad96c2aee0b54bd2f8243cdcdb4d86d016667
mekscodes <meksgit@gmail.com>
2023-08-29 15:31:20 +0100

    Add templates/hello

--- firstDjangoProject/hello/views.py
+++ firstDjangoProject/hello/views.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+# def index(request):
+#     return HttpResponse("Hello, world 1!")
 
 def brian(request):
     return HttpResponse("Hello, Brian!")  
     
+age=2037-1987
 def greet(request,name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request,"hello/greet.html",{
+        "name": name.capitalize(),
+    })
 
+def index(request):
+    return render(request,"hello/index.html")
